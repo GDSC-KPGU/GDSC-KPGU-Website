8335e5e630112230da30c04d77007d9077a6d59a
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-09-29 21:44:16 +0530

    fix: Color

--- src/componets/common/Footer.jsx
+++ src/componets/common/Footer.jsx
@@ -6,7 +6,7 @@ import Logo from "../../assets/Gdsclogo.png";
 export default function Footer() {
   return (
     <div>
-      <footer class="bg-white dark:bg-gray-900 h-full">
+      <footer class="bg-white dark:bg-[#1F1F1F] h-full">
         <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div class="md:flex md:justify-between">
             <div class="mb-6 md:mb-0">
